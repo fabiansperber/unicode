55d626f6b7e76158f1dcd64ca29ef24f18f78da5
Fabian Sperber <fabian.sperber@gmx.net>
2017-12-21 17:16:54 +0100

    initial

---
+++ include/unicode.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace unicode
+{
+// std::string to_utf8(std::u16string_view);
+// std::string to_utf8(std::u32string_view);
+
+// std::u16string to_utf16(std::u16string_view);
+// std::u16string to_utf16(std::u32string_view);
+
+// std::u32string to_utf32(std::u16string_view);
+// std::u32string to_utf32(std::u32string_view);
+}
