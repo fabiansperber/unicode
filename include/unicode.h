119b343bebf6cca2ca9dab6844ce55ac38fb413c
Fabian Sperber <fabian.sperber@gmx.net>
2017-12-28 00:54:37 +0100

    Fix compiler warning

--- include/unicode.h
+++ include/unicode.h
@@ -5,7 +5,7 @@
 
 namespace unicode
 {
-bool get_next_char_from_utf8(std::string_view &text, char32_t &character);
+static inline bool get_next_char_from_utf8(std::string_view &text, char32_t &character);
 struct Codepoints
 {
   struct iterator {
@@ -30,10 +30,10 @@ struct Codepoints
   iterator begin() const { return { text }; }
   iterator end() const { return {}; }
 };
-std::string to_utf8(std::u16string_view);
+static inline std::string to_utf8(std::u16string_view);
 // std::string to_utf8(std::u32string_view);
 
-std::u16string to_utf16(std::string_view src);
+static inline std::u16string to_utf16(std::string_view src);
 // std::u16string to_utf16(std::u32string_view);
 
 // std::u32string to_utf32(std::u16string_view);
